f22926f50c32bc76a3ee853e3134b10c72053179
GabrielMensor <gabriel.264121@alunosatc.edu.br>
2025-10-02 21:26:31 -0300

    Criação do exercício 01 da lista 10

---
+++ Lista010/exercicio01-lista010/src/main/java/com/example/exercicio01_lista010/Exercicio01Lista010Application.java
@@ -0,0 +1,13 @@
+package com.example.exercicio01_lista010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio01Lista010Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio01Lista010Application.class, args);
+	}
+
+}
---
+++ Lista010/exercicio01-lista010/src/test/java/com/example/exercicio01_lista010/Exercicio01Lista010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio01_lista010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio01Lista010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
